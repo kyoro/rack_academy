405f51a9b561ebde15632958ae6c08ca12beac93
Kyosuke INOUE <kyoro@hakamastyle.net>
2012-03-19 13:50:33 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RackAcademy::Application.config.secret_token = '13c87c105cc19a51cdb303b40db34f98f9b966b677007d2a3df516bb36d5f7a15d0db12d2706e47fb24bc7ca0b44b4fbfc0abc0cf95704c933638fce94818f68'
