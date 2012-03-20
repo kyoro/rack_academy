324341f872ed8d34cfaeef6f8e5d6ad786293e7e
Kyosuke INOUE <kyoro@hakamastyle.net>
2012-03-19 21:23:23 -0700

    dist

--- app/models/form/lesson.rb
+++ app/models/form/lesson.rb
@@ -2,11 +2,18 @@
 require 'simple_validator'
 
 class Form::Lesson < CachedForm
-  attr_accessor :title, :description, :video, :script
+  attr_accessor :title, :description, :video, :script, :passcode
 
-  validate :not_blank?
+  validate :not_blank?,:passcode?
 
   def not_blank?
+  end
+
+  def passcode?
+    if passcode != "miko3moemoe"
+      errors.add(:passcode,"passcode invalid")
+      return
+    end
   end
 
 end
